b36cb3e9f4a0dad5096b433befc16d6cf408e84f
Zlata <zlatata05@gmail.com>
2024-11-28 15:17:39 +0200

    Add sounds and sounds settings

--- unityltteam-interviewtask-mantas-e52a3f770b81/Assets/Scripts/GameFlow/GameManager.cs
+++ unityltteam-interviewtask-mantas-e52a3f770b81/Assets/Scripts/GameFlow/GameManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private Image settings;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider effectsSlider;
+    [SerializeField] private AudioMixer audioMixer;
     public void Play()
     {
         SceneManager.LoadScene(1);
@@ -21,12 +25,22 @@ public class GameManager : MonoBehaviour
     {
         settings.gameObject.SetActive(false);
     }
+    public void SetMusicVolume(float volume)
+    {
+        volume = musicSlider.value;
+        audioMixer.SetFloat("Music", volume);
+    }
+    public void SetEffectsVolume(float volume)
+    {
+        volume = effectsSlider.value;
+        audioMixer.SetFloat("Effects", volume);
+    }
     public void Pause()
     {
-
+        Time.timeScale = 0.0f;
     }
     public void Resume()
     {
-
+        Time.timeScale = 1.0f;
     }
 }
